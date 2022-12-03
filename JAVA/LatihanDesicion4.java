cad9f7179a08ba5056fc72e8720dd54a92ab95a8
Elifele Nathan <117702636+ElifeleNathan@users.noreply.github.com>
2022-12-03 18:21:10 +0800

    Update LatihanDesicion4.java

--- JAVA/LatihanDesicion4.java
+++ JAVA/LatihanDesicion4.java
@@ -1,19 +1,5 @@
 import java.util.Scanner;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author MSI_2022
- */
 public class LatihanDesicion4 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
         int surat,x,hasil,akhir;
