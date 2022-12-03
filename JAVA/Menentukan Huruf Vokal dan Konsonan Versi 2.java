d129244e03497923a76cc3fd2d50a6e8100eb1f6
Elifele Nathan <117702636+ElifeleNathan@users.noreply.github.com>
2022-12-03 18:20:27 +0800

    Project IF/ELSE (if ; else if ; else)

--- JAVA/Menentukan Huruf Vokal dan Konsonan Versi 2.java
+++ JAVA/Menentukan Huruf Vokal dan Konsonan Versi 2.java
@@ -1,20 +1,5 @@
-
 import java.util.Scanner;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author MSI_2022
- */
 public class CobaanVokalKonsonan {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
         String huruf;
