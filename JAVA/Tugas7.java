aba3ea83b237cc39dd3513e5b5299a825107bfe8
Elifele Nathan <117702636+ElifeleNathan@users.noreply.github.com>
2023-04-05 11:43:57 +0800

    Add files via upload

---
+++ JAVA/Tugas7.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Tugas7 {
+    public static void main(String[] args) {
+      double v,t,p,l,j;
+      double phi = 3.14;
+      Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan panjang jari-jari tabung = ");
+        j = sc.nextDouble();
+        System.out.print("Masukkan tinggi tabung = ");
+        t = sc.nextDouble();
+        v = phi*j*j*t;
+        l = 2*phi*j*(j+t);
+        System.out.println("Volume tabung = "+v);
+        System.out.println("Luas selimut tabung = "+l);
+    }
+    
+}
