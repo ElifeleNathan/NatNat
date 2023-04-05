388eff2be6581a559793d6fb4473481d47a6c38c
Elifele Nathan <117702636+ElifeleNathan@users.noreply.github.com>
2023-04-05 11:47:49 +0800

    Add files via upload

---
+++ JAVA/Tugas11.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class Tugas11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int angka;
+        System.out.print("Masukkan Angka = ");
+        angka = sc.nextInt();
+        if (angka == 0)
+        {
+            System.out.println("Bilangan Nol");
+        }
+        else if (angka % 2 == 0)
+                {
+                    System.out.println("Bilangan Genap");
+                }
+        else if (angka < 0)
+        {
+            System.out.println("Error");
+        }
+        else
+        {
+            
+            System.out.println("Bilangan Ganjil");
+        }
+    }
+    
+}
