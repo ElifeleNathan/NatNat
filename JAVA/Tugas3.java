96ea4325684de3cd9273abfd8a7e138332200986
Elifele Nathan <117702636+ElifeleNathan@users.noreply.github.com>
2023-04-05 11:37:13 +0800

    Add files via upload

---
+++ JAVA/Tugas3.java
@@ -0,0 +1,15 @@
+public class Tugas3 {
+    public static void main(String[] args) {
+        int count=0;
+        for (int i = 1 ; i<=100 ; i++)
+        {
+            if (i%2==0)
+            {
+                System.out.println(i);
+                count++;
+            }
+        }
+        System.out.println("Jumlah Bil. Genap dari 1 - 100 = "+count);
+    }
+    
+}
